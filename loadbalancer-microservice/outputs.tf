5a02590cfd10da770aab90ff2c8442babab5e650
paganinicaprice <paganinigott@proton.me>
2025-03-06 09:19:22 +0100

    Moved all projects to fundamentals

--- loadbalancer-microservice/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "nginx_lb_static_ip" {
-  value = aws_eip.nginx_eip.public_ip
-}
--- loadbalancer-microservice/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "aws_region" {
-  default = "us-east-1"
-}
-
-variable "key_name" {
-  default = "my-aws-key-new"
-}
